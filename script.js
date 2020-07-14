a3256bafdb62c420b4bca177e4823eba3e7cd31a
maab2b <maab2b@gmail.com>
2020-07-14 18:18:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let numberVar = 28;
+let stringVar = "28";
+let booleanVar = true;
+
+alert(`Переменная numberVar имеет тип - ${typeof numberVar}`);
+alert(`Переменная stringVar имеет тип - ${typeof stringVar}`);
+alert(`Переменная booleanVar имеет тип - ${typeof booleanVar}`);
